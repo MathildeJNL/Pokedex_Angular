1a72c4e1973af504a18d12a863b727a8596b996b
Mathilde Jnl <mathilde.jeannolle@gmail.com>
2023-06-23 11:44:52 +0200

    Update app.component.html, app.component.ts, and 6 more files...

---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,29 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[pkmnBorderCard]'
+})
+export class BorderCardDirective {
+
+  constructor(private el: ElementRef) {
+    this.setHeight(180);
+    this.setBorder('#FF0000');
+  }
+
+  @HostListener('mouseenter') onMouseEnter(){
+    this.setBorder('#009688');
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.setBorder('#F5F5F5');
+  }
+
+  setHeight(height: number){
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+
+  setBorder(color: string){
+    this.el.nativeElement.style.border = `solid 4x ${color}`;
+  }
+
+}
